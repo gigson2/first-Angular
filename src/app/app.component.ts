43b50c011e66f300d074eac00a65e2437a30a3a8
gigson2 <2gigson@gmail.com>
2024-09-23 14:19:41 +0000

    21% done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DesignationComponent } from './components/designation/designation.component';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RolesComponent,DesignationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
