0c10c926d3334951304727097756b6f84a9ff41c
gigson2 <2gigson@gmail.com>
2024-09-25 10:18:34 +0000

    http injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DesignationComponent } from './components/designation/designation.component';
 import { RolesComponent } from './components/roles/roles.component';
+import { MasterComponent } from './components/master/master.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RolesComponent,DesignationComponent],
+  imports: [RouterOutlet,RolesComponent,DesignationComponent,MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -8,24 +10,53 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
- // creating variables in typescript
-firstName: string = "John";
+export class RolesComponent implements OnInit {
 
-version = "18.2.5";
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
+  // creating a constructor
 
-newVersion:number= 18.2;
 
-isActive:boolean = true;
+  // creating a lifecycle event using ngOnInit for dependency injection. dependency injection is used to create a component in angular.
+  ngOnInit(): void {
+    this.getallRoles()
+  }
 
-currentDate:Date = new Date();
-inputType:string = "text";
-inputType2:string = "number";
-inputType3:string = "password";
-inputType4:string = "checkbox";
+  getallRoles() {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res: any) => {
+      this.roleList = res.data;
+    })
+  }
 
-// property binding
+  // // creating variables in typescript
+  // firstName: string = "John";
 
-selectState:string='';
+  // version = "18.2.5";
 
+  // newVersion: number = 18.2;
+
+  // isActive: boolean = true;
+
+  // currentDate: Date = new Date();
+  // inputType: string = "text";
+  // inputType2: string = "number";
+  // inputType3: string = "password";
+  // inputType4: string = "checkbox";
+
+  // // property binding
+
+  // selectState: string = '';
+
+  // // event binding
+
+
+  // //function
+  // showWelcomeMessage() {
+  //   alert("Welcome to Angular 18.2.5");
+  // }
+  // //function with parameters
+  // showMessage(message: string) {
+  //   alert(message);
+
+  // }
 }
